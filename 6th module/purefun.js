68f0d88646e38e0d2d6ab086d0ec9246a8339d01
afiffanoor <Nafiffa28@gmail.com>
2024-04-01 21:03:27 -0500

    changes

--- 6th module/purefun.js
+++ 6th module/purefun.js
@@ -2,7 +2,7 @@
 //impure function
 var a = 10
 function addImpure(x) {
-    console.log(x+a);
+    console.log(x + a);
     a++;
 }
 
@@ -13,21 +13,21 @@ addImpure(2)
 //pure function should return same input and same output
 // donot use any external factor that can effect your code as impure funtion
 
-function addPure(x, a){
-    console.log(x+a);
+function addPure(x, a) {
+    console.log(x + a);
 
 }
-addPure(2,3)
-addPure(2,3)
-addPure(2,3)
-addPure(2,3)
+addPure(2, 3)
+addPure(2, 3)
+addPure(2, 3)
+addPure(2, 3)
 
 //Still the above fucntion is not pure because its using console.log external resource
 //lets write a pure function
 
-function addPures(x , a){
-    return x+a;
+function addPures(x, a) {
+    return x + a;
 }
 
-console.log(addPures(4,5));  //used outside of the function.
+console.log(addPures(4, 5));  //used outside of the function.
 
